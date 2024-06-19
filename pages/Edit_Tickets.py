ce1712d888868aa84fdf141f15c1c5ef39f5c780
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-06-19 17:31:48 +0530

    Update Edit_Tickets.py

--- pages/Edit_Tickets.py
+++ pages/Edit_Tickets.py
@@ -12,43 +12,123 @@ st.title( 'Support Ticket Workflow')
 
 conn = st.connection("supabase",type=SupabaseConnection)
 
-df = execute_query(conn.table("Logs").select("*", count="None"), ttl=None)
-df = pd.DataFrame(df.data)[["id", "created_at", "location", "problem", "engineer", "image", "completed", "completed_at", "call_report"]]
+engineers = list(pd.DataFrame(execute_query(conn.table("Engineers").select("name", count="None"), ttl=None).data)["name"])
+df = execute_query(conn.table("Logs").select("*", count="None"), ttl="0")
+st.write(df)
+if df.count >= 1:
+        df = pd.DataFrame(df.data)[["id", "created_at", "location", "problem", "engineer", "image", "completed", "completed_at", "call_report"]]
 
-event =st.dataframe(df, use_container_width=True, hide_index=True, height=400, 
-        on_select="rerun",
-        selection_mode="single-row",)
+        event =st.dataframe(df, use_container_width=True, hide_index=True, height=400, 
+                on_select="rerun",
+                selection_mode="single-row",column_config={
+                        "id":"ID",
+                        "created_at":st.column_config.DatetimeColumn("Created At"),
+                        "location":"Company - Branch",
+                        "problem":"Problem Statement",
+                        "engineer":"Engineer Name",
+                        "image":st.column_config.ListColumn("Images"),
+                        "completed":st.column_config.CheckboxColumn("Completed"),
+                        "completed_at":st.column_config.DatetimeColumn("Completed At"),
+                        "call_report":st.column_config.ListColumn("Call Reports"),
+                })
 
-if event.selection.rows:
-        people = event.selection.rows
-        filtered_df = df.iloc[people]
-        filtered_df = filtered_df.reset_index()
-        st.title(filtered_df["location"][0])
-        st.selectbox("Created At", options=[datetime.strptime(filtered_df["created_at"][0][0:19], "%Y-%m-%dT%H:%M:%S")+timedelta(hours=5, minutes=30)])
-        
-        st.selectbox("ID", options=[filtered_df["id"][0]])
-        st.selectbox("Location", [filtered_df["location"][0]])
-        st.text_area("Issue",filtered_df["problem"][0])
-        st.selectbox("Engineer", [filtered_df["engineer"][0]])
-        images = filtered_df["image"][0]
-        if images != None:
-                for i in images:
-                        data = conn.download("images" ,i, ttl=None)
-                        st.image(data[0])
-                        delImg = st.button("delete image", key=i)
-                        if delImg:
-                                images.remove(i)
-                                st.rerun()
-        st.file_uploader(label="Problem Images", accept_multiple_files=True, type=["png", "jpeg", "jpg", "webp"])
-        st.toggle("Completed", value=filtered_df["completed"][0])
-        st.date_input(label="Completed At Date", value=datetime.strptime(filtered_df["completed_at"][0][0:10], "%Y-%m-%d") if filtered_df["completed_at"][0] else None)
-        st.time_input(label="Completed At Time", value=datetime.strptime(filtered_df["completed_at"][0][11:19], "%H:%M:%S")+timedelta(hours=5, minutes=30)if filtered_df["completed_at"][0] else None)
-        st.file_uploader(label="Call Report", accept_multiple_files=True, type=["png", "jpeg", "jpg", "webp"])
-        col1, col2 = st.columns([1,1])
-        with col1:
-                st.button("Save", use_container_width=True)
-        with col2:
-                st.button("Delete", use_container_width=True, type="primary")
-        
+        if event.selection.rows:
+                selected_row = df.iloc[event.selection.rows].copy().reset_index()
+                id = selected_row["id"][0]
+                created_at = selected_row["created_at"][0]
+                location = selected_row["location"][0]
+                problem = selected_row["problem"][0]
+                engineer = selected_row["engineer"][0]
+                images = selected_row["image"][0]
+                completed = selected_row["completed"][0]
+                completed_at = selected_row["completed_at"][0]
+                call_report = selected_row["call_report"][0]
 
-        
+                with st.form("Edit"):
+
+                        st.title(location)
+                        st.write(datetime.strptime(created_at[0:19], "%Y-%m-%dT%H:%M:%S")+timedelta(hours=5, minutes=30))
+                        st.write(id)
+
+                        problemInput = st.text_area("Problem Statement", value=problem)
+                        engineerInput = st.selectbox("Engineer", options=engineers, index=engineers.index(engineer))
+
+                        placeholderImage1 = st.empty()
+                        placeholderImage2 = st.empty()
+                        placeholderImage3 = st.empty()
+
+                        completedInput = st.toggle("Completed", completed)
+                        col1Completed, col2Completed = st.columns([1,1])
+                        with col1Completed:
+                                completedDate = st.date_input("Completed At")
+                        with col2Completed:
+                                completedTime = st.time_input("Completed At")
+
+
+                        placeholderCallReport1 = st.empty()
+                        placeholderCallReport2 = st.empty()
+                        placeholderCallReport3 = st.empty()
+
+                        col1Bottom, col2Bottom = st.columns([1,1])
+                        with col1Bottom:
+                                delete = st.form_submit_button("Delete Ticket", type="primary", use_container_width=True)
+                        with col2Bottom:
+                                save = st.form_submit_button("Save Changes", use_container_width=True)
+                
+                with placeholderImage1:
+                        newImageInput = st.file_uploader(label="Images", type=["png", "jpg", "jpeg", "webp"], accept_multiple_files=True)
+                with placeholderImage2:
+                        imageInput = st.multiselect("Images", images+newImageInput, images+newImageInput)
+                with placeholderImage3:
+                        if imageInput != None:
+                                with st.container():
+                                        for i in imageInput:
+                                                if isinstance(i, str):
+                                                        data = conn.download("images" ,source_path=i, ttl=None)
+                                                        st.image(data[0])
+                                                else:
+                                                        st.image(i)
+                with placeholderCallReport1:
+                        newCallReportInput = st.file_uploader(label="Call Reports", type=["png", "jpg", "jpeg", "webp"], accept_multiple_files=True)
+                with placeholderCallReport2:
+                        callReportInput = st.multiselect("Call Reports", call_report+newCallReportInput, call_report+newCallReportInput)
+                with placeholderCallReport3:
+                        if callReportInput != None:
+                                with st.container():
+                                        for i in callReportInput:
+                                                if isinstance(i, str):
+                                                        data = conn.download("images" ,source_path=i, ttl=None)
+                                                        st.image(data[0])
+                                                else:
+                                                        st.image(i)
+
+                if save:
+                        for i in imageInput:
+                                if not isinstance(i, str):
+                                        filename = "images/"+str(datetime.now())+i.__getattribute__("name")
+                                        conn.upload("images", "local",i , filename)
+                                        imageInput.append(filename)
+                                        imageInput.remove(i)
+                        for i in callReportInput:
+                                if not isinstance(i, str):
+                                        filename = "call_reports/"+str(datetime.now())+i.__getattribute__("name")
+                                        conn.upload("images", "local",i , filename)
+                                        callReportInput.append(filename)
+                                        callReportInput.remove(i)
+                
+                        execute_query(conn.table('Logs').update([{
+                                "problem":problemInput,
+                                "engineer":engineer,
+                                "image":imageInput,
+                                "completed":str(completedInput),
+                                "completed_at":str(datetime.combine(completedDate, completedTime)),
+                                "call_report":callReportInput,
+                        }]).eq("id", id), ttl='0')
+                        st.rerun()
+                
+                if delete:
+                        execute_query(conn.table('Logs').delete(count=None).eq("id", id), ttl='0')
+                        st.rerun()
+
+else:
+        st.write("No Records")  
