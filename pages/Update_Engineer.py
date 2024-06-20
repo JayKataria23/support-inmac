869471c640a2696c093347b8e06b659b18e95430
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-06-20 18:00:18 +0530

    Update Update_Engineer.py

--- pages/Update_Engineer.py
+++ pages/Update_Engineer.py
@@ -13,7 +13,6 @@ engineers = list(pd.DataFrame(execute_query(conn.table("Engineers").select("name
 engineerInput = st.selectbox("Engineer", options=engineers, index=None)
 if engineerInput is not "" and engineerInput is not None:
     data = execute_query(conn.table("Engineers").select("*", count="None").eq('name', engineerInput), ttl=None).data[0]
-    st.write(data)
     with st.form('engineer'):
         number = st.text_input('Phone Number*', value=data["contact_number"])
         email = st.text_input('Email ID*', value=data["email"])
@@ -21,7 +20,12 @@ if engineerInput is not "" and engineerInput is not None:
         location = st.text_input('Location', value=data["location"])
         placeholder_for_selectbox = st.empty()
         placeholder_for_optional_text = st.empty()
-        submit = st.form_submit_button('Submit')
+
+        col1Bottom, col2Bottom = st.columns([1,1])
+        with col1Bottom:
+                delete = st.form_submit_button("Delete Ticket", type="primary", use_container_width=True)
+        with col2Bottom:
+                save = st.form_submit_button("Save Changes", use_container_width=True)
 
 
     with placeholder_for_selectbox:
@@ -36,17 +40,21 @@ if engineerInput is not "" and engineerInput is not None:
         else:
             domain = selection
 
-        if submit:
-            if name != "" and number != "" and email != "" and domain != "":
-                st.write(number+name+email+domain)
-                execute_query(conn.table('Engineers').insert([{
+        if save:
+            if number != "" and email != "" and domain != "":
+                execute_query(conn.table('Engineers').update([{
                     "contact_number":number,
-                    "name":name,
                     "email":email,
                     "field":field,
                     "location":location,
                     "domain":domain
-                    }]), ttl='0')
+                    }]).eq('name', engineerInput), ttl='0')
                 st.rerun()
             else:
                 st.write("Please fill all required fields")
+        
+        if delete:
+            execute_query(conn.table('Engineers').delete(count=None).eq('name', engineerInput), ttl='0')
+            st.rerun()
+    
+
