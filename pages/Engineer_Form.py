1b3656b3fc6a838495fed8130765b83554f27f54
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-06-20 16:12:27 +0530

    Update Engineer_Form.py

--- pages/Engineer_Form.py
+++ pages/Engineer_Form.py
@@ -11,9 +11,12 @@ with st.form('engineer'):
     name = st.text_input('Name*', )
     number = st.text_input('Phone Number*')
     email = st.text_input('Email ID*')
+    field = st.toggle("Field Engineer")
+    location = st.text_input('Location')
     placeholder_for_selectbox = st.empty()
     placeholder_for_optional_text = st.empty()
     submit = st.form_submit_button('Submit')
+
 
 with placeholder_for_selectbox:
     selection = st.selectbox("Domain", ["Hardware Engineer", "PM Engineer", "Printer Engineer", "Other"], index=0)
@@ -28,10 +31,12 @@ with placeholder_for_optional_text:
         if name != "" and number != "" and email != "" and domain != "":
             st.write(number+name+email+domain)
             execute_query(conn.table('Engineers').insert([{
-                                                          "contact_number":number,
-                                                          "name":name,
-                                                          "email":email,
-                                                          "domain":domain}]), ttl='0')
+                                                            "contact_number":number,
+                                                            "name":name,
+                                                            "email":email,
+                                                            "field":field,
+                                                            "location":location,
+                                                            "domain":domain}]), ttl='0')
             st.rerun()
         else:
             st.write("Please fill all required fields")
