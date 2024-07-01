3bf5ca96e3b584bda5c4a431af1282c90f1c72dd
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2024-07-01 11:37:09 +0530

    Update Ticket_Form.py

--- pages/Ticket_Form.py
+++ pages/Ticket_Form.py
@@ -3,12 +3,11 @@ import datetime
 import pandas as pd
 from st_supabase_connection import SupabaseConnection, execute_query
 
-
-conn = st.connection("supabase",type=SupabaseConnection)
-
 st.set_page_config(page_title='Support Ticket Workflow', page_icon='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAY1BMVEX////9+PjXjI6+P0K/QkXBSErpv8D14+PJZWi6MTS7NTjFVljryMjCT1G6MjW7Njm5LTDTfoDgqaq4Jir67+/ThIbx19fz3d3doqP25+fIYGPReXvdnqDYkpP79PTluLm+QELIVu6CAAAAy0lEQVR4AX2SBQ7DQAwEHc4xlMP//2TpnNJGHbFW2pGBPsjyokxUNf3StEI+EaqBUBvrnvhAQCxkCncRsv3BplDKI4SnVrgnQmV/lAfIsrPjVlFvKLnVmgsqOw59j8q6TEppIyoHkZS2OqKy9zxIu6FU3OrHCcLZcmtZozJfW7sTKtdBxGFPRN/DHAtWuohTRs9KowkIr0FQORnBp9wYRHOrLGcCzju+iDrilKvS9nsIG7UqB0LlwsqixnCQT5zo8CL7sJRlcUd8v9YNS1IRq/svf5IAAAAASUVORK5CYII=')
 st.image("https://i0.wp.com/inmac.co.in/wp-content/uploads/2022/09/INMAC-web-logo.png?w=721&ssl=1")
 st.title( 'Write a ticket')
+
+conn = st.connection("supabase",type=SupabaseConnection)
 
 locations = list(pd.DataFrame(execute_query(conn.table("Locations").select("name", count="None"), ttl=None).data)["name"])
 engineers = list(pd.DataFrame(execute_query(conn.table("Engineers").select("name", count="None"), ttl=None).data)["name"])
